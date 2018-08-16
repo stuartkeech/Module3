e1b0b2ce282191137b4951c31485e722153f6416
aaric1120 <42442789+aaric1120@users.noreply.github.com>
2018-08-16 17:01:06 -0400

    Updated Database.java
    Included four functions by Chin Han Chen

--- Database.java
+++ Database.java
@@ -76,5 +76,76 @@ public class Database {
     	}
     	return outputA;
     }
+	
+    // created by Chin Han Chen on 2018/08/16
+    public boolean checkDate(String polmID) throws Exception{
+    	Statement st = null;
+    	ResultSet rs = null;
+		double temp1 = 0;
+		Date temp2 = new Date();
+		st = connection.createStatement();
+		rs = st.executeQuery("select Policies.tenure, PolicyMap.start_date "+
+		"from Policies inner join PolicyMap on Policies.policy_id = PolicyMap.policy_ID");
+		while(rs.next()) {
+			temp1 = rs.getDouble(1);
+			temp2 = new Date(rs.getDate(1).getTime());
+		}
+		rs.close();
+		st.close();
+		LocalDate date1 = temp2.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		return(Period.between(date1,LocalDate.now()).getYears() >= temp1);
+	}
+    
+    // created by Chin Han Chen on 2018/08/16
+    public void inputData(int inp2,java.util.Date inp3, int inp5, String inp6, String inp7, Part filePart) throws Exception{
+    	Statement st = null;
+    	PreparedStatement pr = null;
+		pr = connection.prepareStatement("insert into Claims values((select NVL(max(claim_id)+1,1) from Claims),?,?,null,?,?,?,?)");
+		pr.setInt(2, inp2);
+		pr.setDate(3,sqlDate);
+		pr.setInt(5, inp5);
+		pr.setString(6, inp6);
+		pr.setString(7, inp7);
+		if(filePart != null) {
+			InputStream is = filePart.getInputStream();
+			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+			int nRead;
+			byte[] data = new byte[16384];
+			while((nRead = is.read(data,0,data.length))!=-1) {
+				buffer.write(data,0,nRead);
+			}
+			buffer.flush();
+			byte[] filecontents = buffer.toByteArray();
+			Blob b = con.createBlob();
+			b.setBytes(1, filecontents);
+			pr.setBlob(8, b);
+		}else {
+			pr.setBlob(8, (Blob)null);
+		}
+		pr.executeUpdate();
+		pr.close();
+	}
+    
+    // created by Chin Han Chen on 2018/08/16
+    public void inputRejectionReason(String inp, String poliMid) {
+    	PreparedStatement pr = null;
+    	pr = connection.prepareStatement("insert into PolicyMap (reason_for_rejection) values (?) where policy_map_id = ?");
+    	pr.setString(1,inp);
+    	pr.setString(2, poliMid);
+    	pr.executeUpdate();
+    	
+    	pr.close();
+    }
+    
+    // created by Chin Han Chen on 2018/08/16
+    public void inputRejectionStatus(int inp, String poliMid) {
+    	PreparedStatement pr = null;
+    	pr = connection.prepareStatement("update PolicyMap set approved = ? where policy_map_id = ?");
+    	pr.setInt(1,inp);
+    	pr.setString(2, poliMid);
+    	pr.executeUpdate();
+    	
+    	pr.close();
+    }
    
 }
